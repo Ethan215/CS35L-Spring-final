0876b4c96751362d28db3fb67d930cb797ae23a5
Leon Liu <liuleon360@gmail.com>
2024-05-06 20:16:19 -0700

    Add TailwindCSS, sample styling in home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-
+import React from "react";
+import { Link } from "react-router-dom";
 const Home = () => {
-  return (
-    <div>
-      <h1>This is the homepage</h1>
-    </div>
-  );
-}
+	return (
+		<div>
+			<h1>This is the homepage</h1>
+			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Link to="/profile" className="text-blue-500 underline">Go to profile</Link>
+		</div>
+	);
+};
 
 export default Home;
