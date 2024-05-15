e9421c2cf0fd9a9054ba1156a2d8bb3079b2a550
Leon Liu <liuleon360@gmail.com>
2024-05-15 13:00:14 -0700

    Add ESLint configuration and TypeScript dependencies

---
+++ backend/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+	parser: "@typescript-eslint/parser",
+	plugins: ["@typescript-eslint"],
+	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+	rules: {
+		"@typescript-eslint/no-unused-vars": "error",
+		"@typescript-eslint/explicit-module-boundary-types": "error",
+	},
+};
