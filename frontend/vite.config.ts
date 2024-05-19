04ee4f007f863011e8a4c8932ed70e3a7efc6220
Leon Liu <liuleon360@gmail.com>
2024-05-19 00:43:34 -0700

    Fix error with proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,8 +7,9 @@ export default defineConfig({
   server: {
       proxy: {
         '/api': {
-          target: 'http://localhost:4000',
+          target: 'http://localhost:4000/api',
           changeOrigin: true,
+          secure: false,
           rewrite: (path) => path.replace(/^\/api/, ''),
         },
       },
