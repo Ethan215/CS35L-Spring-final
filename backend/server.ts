8e8195d94ff00ec4cb385741cd675f3d3a7fb3dc
Leon Liu <liuleon360@gmail.com>
2024-05-20 23:56:22 -0700

    add user login routes to server.ts

--- backend/server.ts
+++ backend/server.ts
@@ -7,16 +7,25 @@ import mongoose from "mongoose";
 //CORS
 import cors from "cors";
 
+// cookie parser middleware
+import cookieParser from "cookie-parser";
+
 import profileRoutes from "./src/routes/profileRoutes";
+import userRoutes from "./src/routes/userRoutes";
 
 //express app
 const app = express();
 
+
+//middleware
+
 // CORS
 app.use(cors());
 
-//middleware
 app.use(express.json()); //sees if request body if so parses it as json
+
+//cookie parser
+app.use(cookieParser());
 
 // TODO: remove, log incoming requests
 app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
@@ -25,6 +34,9 @@ app.use((req: express.Request, res: express.Response, next: express.NextFunction
 });
 
 // routes:
+
+// route /api/user to userRoutes
+app.use("/api/user", userRoutes);
 
 // route /api/profiles to profileRoutes
 app.use("/api/profiles", profileRoutes);
