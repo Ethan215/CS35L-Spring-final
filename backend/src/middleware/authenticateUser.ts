92d19988c968f7e2baea5bd288c02a6a89c94032
Leon Liu <liuleon360@gmail.com>
2024-05-20 23:56:32 -0700

    add middleware for authenticating a user

---
+++ backend/src/middleware/authenticateUser.ts
@@ -0,0 +1,27 @@
+/// <reference path="../../types.d.ts" />
+//TODO fix tsconfig.json to properly include types
+
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const secretKey: string = process.env.SECRET_KEY!;
+
+export const authenticateUser = (req: Request, res: Response, next: NextFunction): void => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    res.status(401).json({ error: 'Access denied. No token provided.' });
+    return;
+  }
+
+  try {
+    const decoded = verify(token, secretKey) as { userId: string, email: string };
+    req.user = { userId: decoded.userId, email: decoded.email };
+    next();
+  } catch (error) {
+    res.status(400).json({ error: 'Invalid token.' });
+  }
+};
