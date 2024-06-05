6c0be14f8254b513a1e700ab603e3af82c6eb464
scaryre <157163231+scaryre@users.noreply.github.com>
2024-06-05 03:41:40 -0700

    add game icons

--- frontend/src/assets/gameIconDictionary.ts
+++ frontend/src/assets/gameIconDictionary.ts
@@ -1,13 +1,11 @@
 import MinecraftIcon from "./icons/Minecraft.png";
 import LeagueIcon from "./icons/League of Legends.png";
-<<<<<<< HEAD
 import OverwatchIcon from "./icons/Overwatch.png";
 import AboutIcon from "./icons/playing.jpeg";
-=======
-import OverwatchIcon from "./icons/Overwatch.png"
-import VALORANTICON from "./icons/valorant.png"
-
->>>>>>> e5c90ed6e8f4432073ecc4f405e013de42b28698
+import ValorantIcon from "./icons/Valorant.png"
+import RainbowSixIcon from "./icons/Rainbow Six.png"
+import CounterStrikeIcon from "./icons/Counter Strike.png"
+import ApexIcon from "./icons/Apex Legends.png"
 
 interface GameIconDictionary {
 	[key: string]: string;
@@ -17,9 +15,9 @@ export const gameIconDictionary: GameIconDictionary = {
 	Minecraft: MinecraftIcon,
 	"League of Legends": LeagueIcon,
 	Overwatch: OverwatchIcon,
-<<<<<<< HEAD
-	About: AboutIcon
-=======
-	VALORANT: VALORANTICON
->>>>>>> e5c90ed6e8f4432073ecc4f405e013de42b28698
+	About: AboutIcon,
+	VALORANT: ValorantIcon,
+	"Rainbow Six": RainbowSixIcon,
+	"Counter Strike": CounterStrikeIcon,
+	"Apex Legends": ApexIcon
 };
